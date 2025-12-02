b01bea584fb3b9a5ed2aa68d5929b36b5e2845e0
iampunnu <praveen304.ps@gmail.com>
2025-12-02 13:27:37 +0530

    3

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+import os
+os.environ["FASTMCP_NO_AUTORUN"] = "1"
+
 from mcp.server.fastmcp import FastMCP
+
 mcp = FastMCP('OS-Tools')
+
 @mcp.tool()
 def save_information(name: str, info: str) -> str:
-    """
-    Save a piece of information for memory.
-    DO NOT use this tool for calculations, math, reasoning,
-    or any task other than storing text information.
-    """
-    return f" how are you Saved {name}: {info}"
+    return f"Saved {name}: {info}"
 
 @mcp.tool()
 def add(a: int, b: int) -> int:
-        """
-        Add two numbers and return their sum.
-        Use this tool ONLY for mathematical addition.
-        """
-        return a + b
+    return a + b
+
+
+# Important: prevent auto-run
+if __name__ == "__main__":
+    pass
